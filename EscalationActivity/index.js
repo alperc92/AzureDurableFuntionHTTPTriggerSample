0a774fe90b6fce0392ac6babec3a3f19da9d5de2
Alper Cinar <soc.mac92@gmail.com>
2020-11-26 11:35:37 +0000

    added durable func with http trigger, orchestrator and the callees activites Approval and Escalation

---
+++ EscalationActivity/index.js
@@ -0,0 +1,3 @@
+module.exports = async function (context) {
+    return `ESCALATION : You have not approved the project design proposal - reassigning to your Manager!  ${context.bindings.name}!`;
+};
